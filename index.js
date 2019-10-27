490a9690f29e0cd2135437544948f0d12bfc37b8
Sonia Ramiro <soniaramiro0@gmail.com>
2019-10-27 18:59:37 +0100

    express

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
+
+const routes = require('./routes');
 const express = require('express');
 
 // crear una app de express
 const app = express();
 
-// ruta para el home 
-app.use('/' , (request, response) =>{
-    response.send('Hola');
-});
+app.use('/', routes());
 
 // puerto de escucha 
 app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+const producto = [
+    { producto: 'Libro', precio: 20 },
+    { producto: 'PC', precio: 200 }
+]
+
+module.exports = function () {º
+    // ruta para el home 
+    router.get('/', (request, response) => {
+        response.send('Hola');
+        // response.json(producto);
+    });
+
+    return router;
+}
